3801bf8b1f85f4e0768de278188ecec7c23c5a41
JuPrvnt <justine.provent90@gmail.com>
2022-03-02 18:16:09 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const saucesRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
+
+mongoose
+  .connect(
+    "mongodb://usgnvs1hppxhatar2xl7:xRwau1uyvC6za2YfTVxn@brogbj3a6nukl60-mongodb.services.clever-cloud.com:27017/brogbj3a6nukl60",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+// Intercepte toutes les requêtes qui contiennent du JSON
+// pour le mettre à disposition sur l'objet requête dans req.body
+// remplace body parser
+app.use(express.json());
+
+app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
---
+++ routes/sauces.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const saucesCtrl = require("../controllers/sauces");
+const auth = require("../middleware/auth");
+
+router.post("/", auth, saucesCtrl.createSauce);
+router.put("/:id", auth, saucesCtrl.modifySauce);
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.get("/", auth, saucesCtrl.getAllSauce);
+
+module.exports = router;
